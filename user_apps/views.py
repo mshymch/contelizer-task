3ab28c05c99d2770e6e446b203fd4a121a0ea217
admin <test@test.com>
2024-10-08 18:49:13 +0200

    Add file upload page

--- user_apps/views.py
+++ user_apps/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import UploadFileForm
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def text(request):
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # handle_uploaded_file(request.FILES["file"])
+            return HttpResponseRedirect("/success/url/")
+    else:
+        form = UploadFileForm()
+    return render(request, "text.html", {"form": form})
